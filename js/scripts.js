c72d611ac452fce367f651a183bd06f6d5173778
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-18 12:01:37 -0800

    added more styling and start intro to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  $("button#submit").subit(function(event) {
+  $("#questionairre").submit(function(event) {
+
     event.preventDefault();
+
+    var 1 = $("#company").val();
+
+    var 2 = $("#working-end").val();
+
+    var 3 = $("#comfortable").val();
+
+
+  var submit = function(company) {
+      if ((var 1 == select) && (var 2 == select) && (var 3 == select)) {
+                    return "Error";
+                  }
 
 
 
