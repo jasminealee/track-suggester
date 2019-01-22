840dd77d317b73715e3115984cbe1a31a457481e
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-21 16:07:19 -0800

    add revisions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,100 +8,20 @@ $(document).ready(function() {
       if ((q1 == 0) || (q2 == 0) || (q3 == 0)) {
         alert("Error");
       }
-      else if ((q1 == 1) && (q2 == 0) && (q3 == 0)) {
+      else if (q2 == 1) {
         return "works";
       }
-      else if ((q1 == 2) && (q2 == 0) && (q3 == 0)) {
-        return "works";
+      else if (q2 == 2) {
+        return "works2";
       }
-      else if ((q1 == 3) && (q2 == 0) && (q3 == 0)) {
-        return "works";
-      }
-      else if ((q1 == 4) && (q2 == 0) && (q3 == 0)) {
-        return "works";
-      }
-      else if ((q1 == 0) && (q2 == 1) && (q3 == 0)) {
-        return "works";
-      }
-      else if ((q1 == 0) && (q2 == 2) && (q3 == 0)) {
-        return "works";
-      }
-      else if ((q1 == 0) && (q2 == 3) && (q3 == 0)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 4) && (q3 == 0)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 0) && (q3 == 1)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 0) && (q3 == 2)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 0) && (q3 == 3)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 0) && (q3 == 4)) {
-          return "works";
-      }
-      else if ((q1 == 1) && (q2 == 1) && (q3 == 0)) {
-          return "works";
-      }
-      else if ((q1 == 2) && (q2 == 2) && (q3 == 0)) {
-          return "works";
-      }
-      else if ((q1 == 3) && (q2 == 3) && (q3 == 0)) {
-          return "works";
-      }
-      else if ((q1 == 4) && (q2 == 4) && (q3 == 0)) {
-          return "works";
-      }
-      else if ((q1 == 1) && (q2 == 0) && (q3 == 1)) {
-          return "works";
-      }
-      else if ((q1 == 2) && (q2 == 0) && (q3 == 2)) {
-          return "works";
-      }
-      else if ((q1 == 3) && (q2 == 0) && (q3 == 3)) {
-          return "works";
-      }
-      else if ((q1 == 4) && (q2 == 0) && (q3 == 4)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 1) && (q3 == 1)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 2) && (q3 == 2)) {
-          return "works";
-      }
-      else if ((q1 == 0) && (q2 == 3) && (q3 == 3)) {
-          return "works";
-      }
-      else if ((q1 == 2) && (q2 == 4) && (q3 == 4)) {
-          return "works";
-      }
-      else if ((q1 == 1) && (q2 == 1) && (q3 == 1)) {
-          return "works";
-      }
-      else if ((q1 == 2) && (q2 == 2) && (q3 == 2)) {
-          return "works";
-
-      }
-      else if ((q1 == 3) && (q2 == 3) && (q3 == 3)) {
-          return "works";
-      }
-      else if ((q1 == 4) && (q2 == 4) && (q3 == 4)) {
-          return "works";
+      else if (q2 == 3) {
+        return "works3";
       }
       else {
         return "error";
       }
+      return("answer");
     };
-
-
-
-
-    alert("answer");
     event.preventDefault();
   });
 });
