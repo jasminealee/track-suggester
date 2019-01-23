88e951e04a7be76428f987bacc25d404007e0b98
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-22 20:49:34 -0800

    add else if statements that return alerts to question 2 and 5 listed option answers

--- js/scripts.js
+++ js/scripts.js
@@ -3,41 +3,26 @@ $(document).ready(function() {
     var q2 = $("#working-end").val();
     var q5 = $("#ready").val();
 
-    if (q2 == 0 || q5 == 0) {
-      alert("works");
-    } else {
-      alert("Please answer all multiple choice questions.");
+    if (q2 == 0 || q5 == 0 || q5 == 2) {
+      alert("Please reread carefully and answer all multiple choice questions.");
     }
-
-    if (q2 == 1) {
+    else if (q2 == 1) {
       alert("works1");
-    } else {
-      alert("Question 5 must be answered as yes.");
     }
-
-    if (q2 == 2) {
+    else if (q2 == 2) {
       alert("works2");
-    } else {
-      alert("Question 5 must be answered as yes.");
     }
-
-    if (q2 == 3) {
+    else if (q2 == 3) {
       alert("works3");
-    } else {
-      alert("Question 5 must be answered as yes.");
     }
-
-    if (q5 == 1) {
+    else if (q5 == 1) {
       alert("works4");
-    } else {
-      alert("Question 5 must be answered as yes.");
     }
-
-
-    if (q5 == 2) {
-      alert("works5");
-    } else {
-      alert("Question 5 must be answered as yes.");
+    else if (q5 == 2) {
+      alert("works4");
+    }
+    else {
+      alert("error");
     }
 
 
