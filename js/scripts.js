c5e65c2799ba8f525ce25272540bd2c523ca44f9
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-22 19:50:16 -0800

    debugged scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,13 @@
 $(document).ready(function() {
   $("#questionnaire").submit(function(event) {
-    var q1 = $("#company").val();
     var q2 = $("#working-end").val();
-    var q3 = $("#comfortable").val();
+    var q3 = $("#ready").val();
 
-    var answer = function() {
-      if ((q1 == 0) || (q2 == 0) || (q3 == 0)) {
-        alert("Error");
-      }
-      else if (q2 == 1) {
-        return "works";
-      }
-      else if (q2 == 2) {
-        return "works2";
-      }
-      else if (q2 == 3) {
-        return "works3";
-      }
-      else {
-        return "error";
-      }
-      return("answer");
-    };
+    if (q2 == 0) {
+      alert("works");
+    } else {
+      alert("false");
+    }
     event.preventDefault();
   });
 });
