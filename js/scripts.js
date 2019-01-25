e245d73e891d8a9c2f4c065d0a590ca43f429cf4
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-24 20:22:27 -0800

    add more outcomes in script.js else if statements for language results

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,33 @@ $(document).ready(function() {
     var q2 = $("#working-end").val();
     var q5 = $("#ready").val();
 
+
     if (q1 == 0 || q2 == 0 || q5 == 0 || q5 == 2) {
       alert("Please re-read each question carefully. There is a mistake with at least one of your answers.");
     }
-    else if (q1 == 1) && (q2 == 1) {
+    else if (q1 == 1 && q2 == 2) {
       $("#java").fadeIn();
     }
-    else if (q1 == 2 || q2 == 2) {
+    else if (q1 == 2 && q2 == 1) {
+      $("#java").fadeIn();
+    }
+    else if (q1 == 3 && q2 == 1) {
+      $("#java").fadeIn();
+    }
+    else if (q1 == 3 && q2 == 3) {
+      $("#java").fadeIn();
+    }
+    else if (q1 == 2 && q2 == 2) {
       $("#ruby").fadeIn();
     }
-    else if (q1 ==3 || q2 == 3) {
+    else if (q1 == 3 && q2 == 2) {
+      $("#ruby").fadeIn();
+    }
+    else if (q1 == 1 && q2 == 1) {
       $("#c").fadeIn();
     }
-    else if (q5 == 1) {
-      alert("works4");
+    else if (q1 == 2 && q2 == 3) {
+      $("#c").fadeIn();
     }
     else {
       alert("error");
